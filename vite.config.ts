d8e330cb84ec2e644c309ce901fe91bf1e02e28f
Keshav-Var <keshavvarshney2005@gmail.com>
2025-06-21 22:25:01 +0530

    issue fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/hackBuddy/" : "/", 
+  base: mode === "production" ? "/hackBuddy" : "/", 
 
   server: {
     host: "::",
